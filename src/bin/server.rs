4edf313f8f5011d0c249ce1fed38be217e25b8b6
ali-k123 <0alikarami0@gmail.com>
2022-11-24 16:16:58 +0330

    cleanup

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -41,37 +41,3 @@ async fn send_to_user(user_list : Arc<DashMap<u16,Mutex<tokio::io::WriteHalf<Tcp
     w.write_all(&(msg.len() as u16).to_be_bytes()).await.unwrap();
     w.write_all(&msg).await.unwrap();
 }
-
-/* 
-async fn handler(mut socket : TcpStream, addr : SocketAddr, tx : broadcast::Sender<(Vec<u8>,SocketAddr)>){
-    let mut rx = tx.subscribe();
-    loop{
-        let mut msg_size = [0u8;2];
-        tokio::select!(
-            _result = socket.read_exact(&mut msg_size) => {
-                let msg_size = u16::from_le_bytes(msg_size);
-                let mut msg = vec![0;msg_size as usize];
-                socket.read_exact(&mut msg).await.unwrap();
-                tx.send((msg, addr)).unwrap();
-            }
-            result = rx.recv() => {
-                match result{
-                    Ok(r) => {
-                        let (msg, other_addr) = r;
-                        if addr!=other_addr{
-                            let msg_size : u16 = msg.len() as u16;
-                            let msg_size = msg_size.to_le_bytes();
-                            socket.write_all(&msg_size).await.unwrap();
-                            socket.write_all(&msg).await.unwrap();
-                        }
-                    }
-                    Err(_) => {
-                        return
-                    }
-                }
-            }
-        );
-    }
-}
-
-*/
