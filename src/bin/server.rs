a13b614cbddf71aad39ab8af4267ab392b266837
ali-k123 <0alikarami0@gmail.com>
2022-11-24 15:47:15 +0330

    inital commit p2p

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,24 +1,47 @@
 use tokio::net::{TcpListener, TcpStream};
-use std::net::SocketAddr;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::sync::broadcast;
+use tokio::sync::Mutex;
+use dashmap::DashMap;
+use std::sync::Arc;
 
 const IP : &str = "127.0.0.1";
 const PORT : &str = "8888";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (tx, _) = broadcast::channel::<(Vec<u8>,SocketAddr)>(16);
     let listener = TcpListener::bind(format!("{}:{}",IP,PORT)).await?;
+    let user_list : Arc<DashMap<u16,Mutex<tokio::io::WriteHalf<TcpStream>>>> = Arc::new(DashMap::new());
     loop {
-        let (socket,addr) = listener.accept().await?;
-        let tx = tx.clone();
+        let (socket,_)= listener.accept().await?;
+        let  l =user_list.clone();
         tokio::spawn(async move {
-            handler(socket,addr,tx).await;
+            handler(socket, l).await;
         });
     }
 }
 
+async fn handler(socket : tokio::net::TcpStream, user_list : Arc<DashMap<u16,Mutex<tokio::io::WriteHalf<TcpStream>>>>){
+    let (mut r, w) = tokio::io::split(socket);
+    user_list.insert(1,Mutex::new(w));
+    loop{
+        let msg_size = r.read_u16().await.unwrap();
+        let user_id = r.read_u16().await.unwrap();
+        let mut msg = vec![0;msg_size as usize-2];
+        r.read_exact(&mut msg).await.unwrap();
+        let l = user_list.clone();
+        tokio::spawn(async move {
+            send_to_user(l, user_id, msg).await;
+        });
+    }
+}
+
+async fn send_to_user(user_list : Arc<DashMap<u16,Mutex<tokio::io::WriteHalf<TcpStream>>>>,user_id : u16, msg : Vec<u8>){
+    let guard = &*user_list.get(&user_id).unwrap();
+    let mut w = guard.lock().await;
+    w.write_all(&msg).await.unwrap();
+}
+
+/* 
 async fn handler(mut socket : TcpStream, addr : SocketAddr, tx : broadcast::Sender<(Vec<u8>,SocketAddr)>){
     let mut rx = tx.subscribe();
     loop{
@@ -49,3 +72,5 @@ async fn handler(mut socket : TcpStream, addr : SocketAddr, tx : broadcast::Send
         );
     }
 }
+
+*/
